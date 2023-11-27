7006e2204b580533f49658b417b52230118eb646
vojtapetiska <146755492+vojtapetiska@users.noreply.github.com>
2023-11-27 15:55:51 +0100

    Update AirplanePath.cpp

--- AirplanePath.cpp
+++ AirplanePath.cpp
@@ -5,8 +5,5 @@ AirplanePath::AirplanePath(TerrainMap& m, Point start_in, Point finish_in)
 
 bool AirplanePath::find() {
     // Implementace algoritmu pro nalezení nejkratší cesty pro letadlo
-    // Případně můžete využít metody třídy TerrainMap pro získání výškových informací
-    // a implementovat hledání cesty podle specifikací zadání.
-    // Upravte podle potřeby.
     return false;
 }
