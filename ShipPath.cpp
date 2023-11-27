ebaaed343c62983f668c7a4d99c9e588544e94c0
vojtapetiska <146755492+vojtapetiska@users.noreply.github.com>
2023-11-27 15:56:13 +0100

    Update ShipPath.cpp

--- ShipPath.cpp
+++ ShipPath.cpp
@@ -5,9 +5,6 @@ ShipPath::ShipPath(TerrainMap& m, Point start_in, Point finish_in)
 
 bool ShipPath::find() {
     // Implementace algoritmu pro nalezení nejkratší cesty pro loď
-    // Případně můžete využít metody třídy TerrainMap pro získání výškových informací
-    // a implementovat hledání cesty podle specifikací zadání.
-    // Upravte podle potřeby.
     return false;
 }
 
